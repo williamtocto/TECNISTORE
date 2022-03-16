f6f13a2c344db93cca963fea3ab5194250418595
MarlonVelez <marlon.velez.est@tecazuay.edu.ec>
2022-03-15 23:47:42 -0500

    Diseño de las vistas de Sign Up y conexion a la API tecnistore

--- app/src/main/java/com/example/tecnistore/MainActivity.java
+++ app/src/main/java/com/example/tecnistore/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startPresentacion(){
-
+    //
         String urlGif = "https://lh3.googleusercontent.com/57jQwxUNbJzbqp16Ilds16xRKOxmlWfnAjQIvczZm4e1zjPCEeb-5XR4tgfQd77X8oh9ZUfULX216NwL5Gy015MpNCbMIfJ3Wvdrhq0EElxj7zCERVq9rYR529XLwaoj7n2ZDqhkc8aGXbzK0hDmnNvto2GTj04idk1j3g6vm_WP6wuM_sOoYyKMhXGvZBksrStKDLoiZvP78rnNhCBvy1pRA8yYIDQNmw5nf6K6Qs1wa_FBGCCOfNR70l5JAX9xwF6jdUI0yNsWVZaKAEXkVclsTAHBtE7NonTMWacD4XKVy-jYOzxVW-s0v5JeWowpiSqXUBMbqJoWBMjXl2pSwh7g4Wb0kvr3OWqp3vhs6E_Pa8Q-gY7Gohk-VmSGK8_L_BIwPj_Vv4YZ3FxVqM1VDGUgLFebEs-r0lg8M533aPkbnSYiycIk5UXij_AcNYSkCVLxDDrjU0f8ffZzyG2IhNsDIxRuYraONg7jc2f66DiiIndgaofh44iUF6EzJC_StacXiI-1zu94cp9wqwY_xybYaIey80MsStZnRpQ2UtiJpFQOTAOQssJCr2oRcDqqNPzIgkmxWLNvhBqj-5jOnJuMZo7czDVwgvd6_LUK5oPmfMESxEWKUrDZQ2py1Ic9IswS3y1mwUCa4prQXhdUpngrPFWlaE5u-vMVYpPHNLLq5PSc2xi_ngqJEa-Ih7msrVRf3JSlNk5Tl3An9XwQ98ic=w391-h220-no?authuser=0";
         //Agregar implementacion Glide dentro de archivo build.gradle.
         ImageView imageView = (ImageView)findViewById(R.id.gifView);
